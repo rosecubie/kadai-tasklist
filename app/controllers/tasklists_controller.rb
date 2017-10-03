c8a9e1ebeb1ac1612f84d059b731fdd2581e5cbf
rosecubie <alexandros3351@gmail.com>
2017-10-03 14:07:03 +0000

    kadai-tasklist 2

---
+++ app/controllers/tasklists_controller.rb
@@ -0,0 +1,61 @@
+class TasklistsController < ApplicationController
+  before_action :set_tasklist,only:[:show,:edit,:update,:destroy]
+  def index
+    @tasklists= Tasklist.all
+  end
+
+  def show
+  end
+
+  def new
+    @tasklist= Tasklist.new
+  end
+
+  def create
+     @tasklist=Tasklist.new(tasklist_params)
+
+    if @tasklist.save
+      flash[:success] = 'Tasklist が正常に投稿されました'
+      redirect_to @tasklist
+    else
+      flash.now[:danger] = 'Tasklist が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    
+    if @tasklist.update(tasklist_params)
+      flash[:success] = "Tasklistは正常に更新されました"
+      redirect_to @tasklist
+    else
+      flash.now[:danger] = "Tasklistは更新されませんでした"
+      render:edit
+    end
+  end
+
+  def destroy
+    @tasklist.destroy
+    
+    flash[:success]= "Tasklistは正常に削除されました"
+    redirect_to tasklists_url
+  end
+  
+  private
+  
+  def set_tasklist
+    @tasklist= Tasklist.find(params[:id])
+  end
+  
+  def tasklist_params
+    params.require(:tasklist).permit(:content)
+  end
+  
+  def message_params
+    params.require(:message).permit(:content, :status)
+  end
+
+end
