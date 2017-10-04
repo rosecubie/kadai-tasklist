f8b3139bac3b0fb221d542d2a79c00b510c04d88
rosecubie <alexandros3351@gmail.com>
2017-10-04 08:12:16 +0000

    tasklist 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002080302) do
+ActiveRecord::Schema.define(version: 20171004080743) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "content"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20171002080302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
