a7c4a96927a564e04e4f12c6bc4f683919ca4ca6
rosecubie <alexandros3351@gmail.com>
2017-10-15 07:02:37 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task,only:[:show,:edit,:update,:destroy]
   def index
     @task= Task.all.page(params[:page])
@@ -12,14 +13,15 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task=Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
       flash.now[:danger] = 'Taskが投稿されませんでした'
-      render :new
+      render "toppages/index"
     end
   end
 
@@ -53,6 +55,14 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+    
+     def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
+    　end
   end
 
 end
